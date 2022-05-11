fcdb2a02d8de59255229bb716a4dab2e425a7bdb
Makarov <makarowalex48@gmail.com>
2022-05-11 08:44:27 +0400

    Initial commit

---
+++ app/src/main/java/com/example/testmobileapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.testmobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int a;
+    private int b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
